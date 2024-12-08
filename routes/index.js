8ed54ae2de5acf8a3e7af00de3deb00d90483ca8
silgenius <olutademartin@gmail.com>
2024-12-08 08:58:49 -0800

    feat(api): Implement routes for auth

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
-import { getStatus } from '../controllers/AppController'
-import { getStats } from '../controllers/AppController'
-import { postNew } from '../controllers/UsersController'
+import { getStatus } from '../controllers/AppController';
+import { getStats } from '../controllers/AppController';
+import { postNew } from '../controllers/UsersController';
+import { getConnect } from '../controllers/AuthController';
+import { getDisconnect } from '../controllers/AuthController';
+import { getMe } from '../controllers/UsersController';
 
 const express = require('express');
 
@@ -9,5 +12,8 @@ const router = express.Router();
 router.get('/status', getStatus);
 router.get('/stats', getStats);
 router.post('/users', postNew);
+router.get('/connect', getConnect);
+router.get('/disconnect', getDisconnect);
+router.get('/users/me', getMe);
 
 module.exports = router;
