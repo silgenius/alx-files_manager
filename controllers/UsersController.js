60a567f69e78b2827384b3e942f137e7984fbe92
silgenius <olutademartin@gmail.com>
2024-12-08 08:59:50 -0800

    feat(auth): Implement functions to handle authentication

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,7 +1,9 @@
 import dbClient from '../utils/db';
 const crypto = require('crypto');
+import redisClient from '../utils/redis';
+const { ObjectId } = require('mongodb');
 
-const hashPassword = (password) => {
+export const hashPassword = (password) => {
 	const hash = crypto.createHash('sha1');
 	hash.update(password);
 	return hash.digest('hex');
@@ -36,4 +38,20 @@ export async function postNew(req, res) {
 			email: email
 		}
 	)
+}
+
+export async function getMe(req, res) {
+        const token = req.get('X-Token');
+        const key = 'auth_' + token;
+        const user_id = await redisClient.get(key);
+        if (!user_id) {
+                return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        const user = await dbClient.findUser({ _id: new ObjectId(user_id) });
+        if (!user) {
+                return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        return res.json({ email: user.email, id: user._id.toString() })
 }
