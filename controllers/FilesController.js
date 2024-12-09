7a62b3dd35fe1033b7847c2a94c6bb26529cc1c5
silgenius <olutademartin@gmail.com>
2024-12-09 06:55:14 -0800

    refator: modify code to use async/await instead of callbacks

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -2,7 +2,7 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 const { ObjectId } = require('mongodb');
 import { randomString } from './AuthController';
-const fs = require('fs');
+const fs = require('fs').promises;
 
 export async function postUpload(req, res) {
 	const token = req.get('X-Token');
@@ -33,7 +33,7 @@ export async function postUpload(req, res) {
 	}
 
 	if (parentId) {
-		file = await findFile({ parentId: parentId });
+		const file = await dbClient.findFile({ _id: new ObjectId(parentId) });
 		if (!file) {
 			return res.status(400).json({ error: 'Parent not found' });
 		}
@@ -48,11 +48,11 @@ export async function postUpload(req, res) {
 		name: name,
 		type: type,
 		parentId: parentId ? parentId : 0,
-		isPublic: isPublic,
+		isPublic: isPublic ? isPublic : false,
 	}
 
 	if (type === 'folder') {
-		const newFile = await createFile(fileDetails);
+		const newFile = await dbClient.createFile(fileDetails);
 		const { _id, ...resp } = newFile.ops[0];
 		resp['id'] = newFile.insertedId
 		return res.status(200).json(resp);
@@ -60,32 +60,23 @@ export async function postUpload(req, res) {
 
 	const folderPath = '/tmp/files_manager';
 
-	// Create folder path dir if not exist
-	fs.mkdir(folderPath, { recursive: true }, (err) => {
-		if (err) {
-			console.log('Error creating folder');
-			return;
-		};
+	try {
+		// Create folder path dir if not exist
+		await fs.mkdir(folderPath, { recursive: true });
 
-		// Write to file
+		console.log('herre');
+		// Create file containing provided data
 		const fileName = randomString();
-		const filePath = folderPath + fileName;
-		const fileData = Buffer.from(data, 'base64');
-		fs.writeFile(filePath, fileData, (err) => {
-			if (err) {
-				console.log('Error writing to file');
-				return
-			};
-
-			console.log('Writing to file was successful');
-		});
-
+                const filePath = folderPath + '/' + fileName;
+                const fileData = Buffer.from(data, 'base64');
+		await fs.writeFile(filePath, fileData);
 		fileDetails['localPath'] = filePath;
-	});
 
-
-	const newFile = await createFile(fileDetails);
-	const { _id, localPath, ...resp } = newFile.ops[0];
-	resp['id'] = newFile.insertedId
-	return res.status(200).json(resp);
+		const newFile = await dbClient.createFile(fileDetails);
+                const { _id, localPath, ...resp } = newFile.ops[0];
+                resp['id'] = newFile.insertedId;
+                return res.status(200).json(resp);
+	} catch(err) {
+		console.log(`Error occured: ${err}`);
+	}
 }
