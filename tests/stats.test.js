6847d69152c9ce11cc867031c4126e2c1d89a148
silgenius <olutademartin@gmail.com>
2024-12-11 15:10:47 -0800

    test: Implement te4st for db status stats and user creation

--- tests/stats.test.js
+++ tests/stats.test.js
@@ -1,17 +1,17 @@
 const { describe, it } = require('mocha');
-const expect = require('chai').expect;
+const { expect } = require('chai');
 const request = require('request');
 
-describe("returns collection in db stats", function() {
-    it('returns the amount of users and files in db', function() {
-        request('http://0.0.0.0:5000/stats', (err, resp, body) => {
-            if (err) {
-                done(err)
-            }
-            expect(resp.statusCode).to.be.equal(200);
-            expect(body).to.have.property('users');
-            expect(body).to.have.property('files');
-            done();
-        })
-    })
-})
+describe('returns collection in db stats', () => {
+  it('returns the amount of users and files in db', (done) => {
+    request('http://0.0.0.0:5000/stats', (err, resp, body) => {
+      if (err) {
+        done(err);
+      }
+      expect(resp.statusCode).to.be.equal(200);
+      expect(body).to.have.property('users');
+      expect(body).to.have.property('files');
+      done();
+    });
+  });
+});
--- tests/status.test.js
+++ tests/status.test.js
@@ -1,16 +1,16 @@
 const { describe, it } = require('mocha');
-const expect = require('chai').expect;
+const { expect } = require('chai');
 const request = require('request');
 
-describe("Checks db status", function() {
-    it('checks redis and mongo connection status', function(done) {
-        request('http://0.0.0.0:5000/status', (err, resp, body) => {
-            if (err) {
-                done(err);
-            }
-            expect(body).to.deep.equal({"redis":true,"db":true});
-            expect(resp.statusCode).to.equal(200);
-            done();
-        })
-    })
-})
+describe('checks db status', () => {
+  it('checks redis and mongo connection status', () => new Promise((done) => {
+    request('http://0.0.0.0:5000/status', (err, resp, body) => {
+      if (err) {
+        done(err);
+      }
+      expect(JSON.parse(body)).to.deep.equal({ redis: true, db: true });
+      expect(resp.statusCode).to.equal(200);
+      done();
+    });
+  }));
+});
